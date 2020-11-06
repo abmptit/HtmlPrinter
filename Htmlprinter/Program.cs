5657bee9d950719f2f2a9a5ab1925c0e7b785187
Aymen BEN MILED <aymen.ben-miled@momentum-tech.ca>
2020-11-06 15:59:15 +0000

    initial commit

---
+++ Htmlprinter/Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenHtmlToPdf;
+using System;
+using System.IO;
+
+namespace Htmlprinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var htmlContent = File.ReadAllText(Path.Combine(".", "LeaderTemplate.html"));
+
+            var reportPdfFileInfo = new FileInfo(Path.Combine(".", "LeaderTemplate.pdf"));
+            var pdfContent = Pdf.From(htmlContent).Portrait().Content();
+            File.WriteAllBytes(reportPdfFileInfo.FullName, pdfContent);
+        }
+    }
+}
